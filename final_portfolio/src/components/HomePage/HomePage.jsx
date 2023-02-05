b93c35d39b0a499ef35ac31809ce81e8622bc39c
SethStephens32 <sethstephens2018@gmail.com>
2023-02-04 19:38:42 -0600

    made changes to the home page and to the skills section

--- final_portfolio/src/components/HomePage/HomePage.jsx
+++ final_portfolio/src/components/HomePage/HomePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState, useEffect } from "react";
 import CodeScreen from '../CodeScreen/CodeScreen'
 import './HomePage.scss'
 import {GrReactjs} from 'react-icons/gr'
@@ -13,36 +14,23 @@ import {SiExpress} from 'react-icons/si'
 import {SiPostgresql} from 'react-icons/si'
 import {SiMongodb} from 'react-icons/si'
 import {SiPython} from 'react-icons/si'
+import Skills from '../Skills/Skills';
 
 
 const HomePage = () => {
+
   return (
     <>
     <section className="home-page">
         <div className="home-page__title">
-        <h1><span className='span-color'>Unleashing</span> technology, one line of code at a time. I'm a <span className='span-color'>software developer</span> with four years of experience. Let's build something great <span className='span-color'>together</span>.</h1>
-        <p>Technical know-how meets <span className='span-color-2'>creativity</span>. Results-driven solutions for <span className='span-color-2'>your</span> next project.</p>
+       <h1>Bringing visionary concepts <br/> to tangible reality is <br/>my <span className='text-gradient'>passion</span>.</h1>
+        <p>Technical know-how meets <span className='text-gradient'>creativity</span>. Results-driven solutions.</p>
         </div>
     </section>
 
     <section className="home-page-about">
         <div className="home-page-about__skills">
-            <GrReactjs className="home-page-about__skills__icon"/>
-            <SiRedux className="home-page-about__skills__icon"/>
-
-            <IoLogoJavascript className="home-page-about__skills__icon"/>
-            <AiFillHtml5 className="home-page-about__skills__icon"/>
-            <DiCss3 className="home-page-about__skills__icon"/>
-            <SiSass className="home-page-about__skills__icon"/>
-
-            <IoLogoNodejs className="home-page-about__skills__icon"/>
-            <DiNpm className="home-page-about__skills__icon"/>
-            <SiExpress className="home-page-about__skills__icon"/>
-
-            <SiPostgresql className="home-page-about__skills__icon"/>
-            <SiMongodb className="home-page-about__skills__icon"/>
-            <SiPython className="home-page-about__skills__icon"/>
-
+        <Skills/>
         </div>
     <CodeScreen/>
     </section>
