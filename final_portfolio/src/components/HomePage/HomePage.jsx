f942e12cea2df5edaeeaddcec1be0a2a40791be8
SethStephens32 <sethstephens2018@gmail.com>
2023-02-10 10:19:32 -0600

    created the about section

--- final_portfolio/src/components/HomePage/HomePage.jsx
+++ final_portfolio/src/components/HomePage/HomePage.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import { motion } from 'framer-motion'
+import { motion, useAnimation } from 'framer-motion'
+import ProfilePicture from '../../assets/profilePicture.jpg'
 import './HomePage.scss'
-import Projects from '../Projects/Projects'
-import CodeScreen from '../CodeScreen/CodeScreen'
 
-const HomePage = () => {
-
+const HomePage = () => {  
   return (
     <>
     <section className="home-page">
@@ -17,6 +15,17 @@ const HomePage = () => {
         animate={{ x: 0, opacity: 1 }}
         transition={{ duration: 2, ease: 'easeOut' }}>Technical know-how meets <span className='text-gradient'>creativity</span>. Results-driven solutions.</motion.p>
         </div>
+    </section>
+    <section className="about">
+      <div className="about__container">
+        <div className="about__title">
+          <h1 className='title'><span className='number-span'>01.</span>About me</h1>
+        </div>
+        <p>Hello stranger! <span>👋</span>, my name is Seth Stephens and I am a <span className="text-gradient">frontend engineer</span>, passionate about creating digital experiences that enhance people's lives and make their daily tasks easier and more enjoyable. <span className="text-gradient">I love bringing ideas to life through clean and intuitive design, seamless functionality, and innovative technologies</span>. As a frontend developer, I am constantly seeking new challenges and opportunities to create user-centric solutions that are both beautiful and functional.</p>
+      </div>
+      <div className="about__image">
+          <img src={ProfilePicture} alt="Profile Picture" className='profilePicture'/>
+      </div>
     </section>
     </>
   )
